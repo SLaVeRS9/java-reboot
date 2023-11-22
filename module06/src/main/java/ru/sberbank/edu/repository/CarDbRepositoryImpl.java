7dcaf928ca3d22d101f74a61bd5f63b1c914611c
SLaVeRS9 <slavers9@outlook.com>
2023-11-22 03:13:39 +0300

    Module06_Task1 Done

--- module06/src/main/java/ru/sberbank/edu/repository/CarDbRepositoryImpl.java
+++ module06/src/main/java/ru/sberbank/edu/repository/CarDbRepositoryImpl.java
@@ -7,23 +7,37 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CarDbRepositoryImpl implements CarRepository {
     private final Connection connection;
     private static final String CREATE_CAR_SQL = "INSERT INTO car (id, model) VALUES (?,?)";
     private static final String UPDATE_CAR_SQL = "UPDATE car SET model = ? WHERE id = ?";
     private static final String SELECT_CAR_BY_ID = "SELECT * FROM car WHERE id = ?";
+    private static final String DELETE_CAR_BY_ID = "DELETE FROM car WHERE id = ?";
+    private static final String FIND_ALL_CARS_SQL = "SELECT * FROM car";
+    private static final String DELETE_ALL_CARS_SQL = "DELETE FROM car";
+    private static final String FIND_CAR_BY_MODEL = "SELECT * FROM car WHERE model = ?";
 
     private final PreparedStatement createPreStmt;
     private final PreparedStatement updatePreStmt;
     private final PreparedStatement findByIdPreStmt;
+    private final PreparedStatement deleteByIdPreStmt;
+    private final PreparedStatement findAllPreStmt;
+    private final PreparedStatement deleteAllPreStmt;
+    private final PreparedStatement findByModelPreStmt;
+    private PreparedStatement createAllPreStmt;
 
     public CarDbRepositoryImpl(Connection connection) throws SQLException {
         this.connection = connection;
         this.createPreStmt = connection.prepareStatement(CREATE_CAR_SQL);
         this.updatePreStmt = connection.prepareStatement(UPDATE_CAR_SQL);
         this.findByIdPreStmt = connection.prepareStatement(SELECT_CAR_BY_ID);
+        this.deleteByIdPreStmt = connection.prepareStatement(DELETE_CAR_BY_ID);
+        this.findAllPreStmt = connection.prepareStatement(FIND_ALL_CARS_SQL);
+        this.deleteAllPreStmt = connection.prepareStatement(DELETE_ALL_CARS_SQL);
+        this.findByModelPreStmt = connection.prepareStatement(FIND_CAR_BY_MODEL);
     }
 
     @Override
@@ -59,9 +73,17 @@ public class CarDbRepositoryImpl implements CarRepository {
         return Optional.of(car);
     }
 
+    /**
+     * Delete car by id
+     * @param id - car id
+     * @return true - if car has been deleted, false - if not
+     * @throws SQLException
+     */
     @Override
-    public Boolean deleteById(String id) {
-        return null;
+    public Boolean deleteById(String id) throws SQLException {
+        deleteByIdPreStmt.setString(1, id);
+        int affectedRows = deleteByIdPreStmt.executeUpdate();
+        return affectedRows > 0;
     }
 
     private int countRowsById(String id) throws SQLException {
@@ -73,5 +95,88 @@ public class CarDbRepositoryImpl implements CarRepository {
             rowCount = resultSet.getInt(1);
         }
         return rowCount;
+    }
+
+    /**
+     * Add cars to DB
+     * @param cars - cars to add
+     * @return Cars set which has been added to DB
+     * @throws SQLException
+     */
+    @Override
+    public Set<Car> createAll(Collection<Car> cars) throws SQLException {
+
+        Set<Car> createdCars = new HashSet<>();
+        if (cars.isEmpty())
+            return createdCars;
+
+        StringBuilder query = new StringBuilder("INSERT INTO car (id, model) VALUES");
+
+        cars.forEach(car -> {
+                query.append(" (" + "'" + car.getId() + "'" + ", " + "'" + car.getModel() + "'" + "),");
+        });
+        query.deleteCharAt(query.length()-1);
+        createAllPreStmt = connection.prepareStatement(query.toString());
+        createAllPreStmt.executeUpdate();
+
+        /* Вариант 2. Спам кучей запросов на создание
+        cars.forEach(car -> {
+            try {
+                createOrUpdate(car);
+                createdCars.add(car);
+            } catch (SQLException e) {
+                System.out.print("log note:");
+                System.out.print("createAll failed on car = " + car + " by reason " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        });*/
+
+        return new HashSet<>(cars);
+    }
+
+    /**
+     * Find and return all cars
+     * @return All cars set in DB
+     * @throws SQLException
+     */
+    @Override
+    public Set<Car> findAll() throws SQLException {
+        Set<Car> allCars = new HashSet<>();
+        ResultSet resultSet = findAllPreStmt.executeQuery();
+        while (resultSet.next()) {
+            String id = resultSet.getString("id");
+            String model = resultSet.getString("model");
+            allCars.add(new Car(id, model));
+        }
+        return allCars;
+    }
+
+    /**
+     * Delete all cars from DB
+     * @return true - if DB has been empty, false - if DB hasn't been empty
+     * @throws SQLException
+     */
+    @Override
+    public Boolean deleteAll() throws SQLException {
+        deleteAllPreStmt.executeUpdate();
+        return findAll().isEmpty();
+    }
+
+    /**
+     * Find and return cars by model
+     * @param model - car model
+     * @return Cars set with the transmitted model
+     * @throws SQLException
+     */
+    @Override
+    public Set<Car> findByModel(String model) throws SQLException {
+        findByModelPreStmt.setString(1, model);
+        ResultSet resultSet = findByModelPreStmt.executeQuery();
+        Set<Car> carsWithModel = new HashSet<>();
+        while (resultSet.next()) {
+            String id = resultSet.getString("id");
+            carsWithModel.add(new Car(id, model));
+        }
+        return carsWithModel;
     }
 }
