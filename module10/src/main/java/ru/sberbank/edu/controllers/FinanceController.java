319743522a26e1cc94b652a942aa58b77c52e42f
SLaVeRS9 <slavers9@outlook.com>
2023-12-16 13:26:20 +0300

    Module10_Task1 little improve in FinanceController

--- module10/src/main/java/ru/sberbank/edu/controllers/FinanceController.java
+++ module10/src/main/java/ru/sberbank/edu/controllers/FinanceController.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.sberbank.edu.services.CalculateService;
 
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/finance")
 public class FinanceController {
     private final CalculateService calculateService;
-    @GetMapping("/finance")
+    @GetMapping()
     public String calculator() {
         return "finance/calculator";
     }
 
-    @PostMapping("/finance")
+    @PostMapping()
     public String calcResult(
             @RequestParam("sum") String sum,
             @RequestParam("percentage") String percentage,
